cd64b8c95943b8830663a49c9ae35841c6b386d9
VladislavSnigarenko <36262031+VladislavSnigarenko@users.noreply.github.com>
2018-02-12 15:55:08 +0200

    Update Phone.java

--- Lesson_1_home/src/Task_4/Phone.java
+++ Lesson_1_home/src/Task_4/Phone.java
@@ -37,9 +37,17 @@ public class Phone {
 		else if (network.getAllNumbersNetwork().contains(number) && number == this.number)
 			System.out.println("Error : Network : " + network.getName() + " exist number : (" + number + "), but call is forbidden to myself (" + this.number + ")");
 		else if (network.getAllNumbersNetwork().contains(number))
+		{
 			System.out.println("Network : " + network.getName() + " exist number : (" + number + "), calling from : (" + this.number + ") to : (" + number + ")");
+			new Phone(number).incomingCall(this.number);
+		}
 		else 
 			System.out.println("Error : Network : " + network.getName() + " not exist number : (" + number + ")");
+	}
+	
+	public void incomingCall(String incomingNumber) {
+		System.out.println("Incoming Call : from incoming number : ("
+				+ incomingNumber + "), to number (" + number + ")");
 	}
 
 }
